d8e7cc5a571d084a60520316f8d0faf62fddc925
alivemaster <alivemaster3@gmail.com>
2024-02-27 12:17:25 +0800

    fix: add unit for font size

--- src/cover/loadFonts.ts
+++ src/cover/loadFonts.ts
@@ -1,8 +1,8 @@
-export default async (): Promise<void> => {
+export default async () => {
     const fonts = [
-        "700 0 'Montserrat Variable'",
-        "700 0 'Quicksand Variable', 'Noto Sans SC Variable', 'Noto Sans TC Variable', 'Noto Sans JP Variable', 'Noto Sans KR Variable'",
-        "400 0 'Quicksand Variable', 'Noto Sans SC Variable', 'Noto Sans TC Variable', 'Noto Sans JP Variable', 'Noto Sans KR Variable'"
+        "700 0px 'Montserrat Variable'",
+        "700 0px 'Quicksand Variable', 'Noto Sans SC Variable', 'Noto Sans TC Variable', 'Noto Sans JP Variable', 'Noto Sans KR Variable'",
+        "400 0px 'Quicksand Variable', 'Noto Sans SC Variable', 'Noto Sans TC Variable', 'Noto Sans JP Variable', 'Noto Sans KR Variable'"
     ]
     for (const font of fonts)
         await document.fonts.load(font)
