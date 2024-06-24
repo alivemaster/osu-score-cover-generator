dc0e8b9a72968eaa6c33452bb4de2c37af614f38
alivemaster <alivemaster3@gmail.com>
2024-06-25 01:24:25 +0800

    add autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,15 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({
+          overrideBrowserslist: ['>0.3%, last 2 major versions, not dead']
+        })
+      ]
     }
   }
 })
