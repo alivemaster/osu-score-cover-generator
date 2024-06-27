52ee188135eb4fda645f876905b8bbaf0abf3bd3
alivemaster <alivemaster3@gmail.com>
2024-06-27 18:36:04 +0800

    add header & remove body margin

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import CoverGenerator from './CoverGenerator.vue'
+import Header from './components/Header.vue'
 </script>
 <template>
+  <Header></Header>
   <CoverGenerator></CoverGenerator>
 </template>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header></header>
+</template>
+<style scoped>
+header {
+    /* box */
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: row;
+    height: 6rem;
+    margin: 0 1rem 1.5rem;
+
+    /* visual */
+    background-color: var(--bg3);
+    border: .03125rem solid var(--stroke);
+    border-radius: 0 0 1.5rem 1.5rem;
+}
+
+@media screen and (width < 1080px) {
+    header {
+    /* box */
+    height: 4rem;
+    margin: 0;
+
+    /* visual */
+    border-radius: 0;
+}
+}
+</style>
