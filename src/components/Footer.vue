1fd951d8f261aa56a0f2e4fe3d47db6e69e01a96
alivemaster <alivemaster3@gmail.com>
2024-06-30 19:09:21 +0800

    complete Header & add Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer>
+        <span>
+            <p>This webpage is not affiliated with "osu!" or "ppy".</p>
+            <p>Flag icons are taken from Twemoji. Twemoji by Twitter are licenced under CC-BY 4.0.</p>
+        </span>
+    </footer>
+</template>
+<style scoped>
+footer {
+    /* box */
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    height: 12rem;
+    padding: 0 3rem;
+}
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,12 @@
 <template>
-    <header></header>
+    <header>
+        <span id="logo">
+            <img src="../assets/logo.svg" style="height: 6rem" alt="osu score cover generator">
+        </span>
+        <a id="github-link" href="https://github.com/alivemaster/osu-score-cover-generator" target="_blank">
+            <img src="../assets/github-mark-white.svg" style="height: 3rem" alt="github logo">
+        </a>
+    </header>
 </template>
 <style scoped>
 header {
@@ -7,23 +14,17 @@ header {
     box-sizing: border-box;
     display: flex;
     flex-direction: row;
-    height: 6rem;
-    margin: 0 1rem 1.5rem;
-
-    /* visual */
-    background-color: var(--bg3);
-    border: .03125rem solid var(--stroke);
-    border-radius: 0 0 1.5rem 1.5rem;
+    align-items: center;
+    justify-content: space-between;
+    height: 8rem;
+    padding: 0 3rem;
 }
 
 @media screen and (width < 1080px) {
     header {
-    /* box */
-    height: 4rem;
-    margin: 0;
-
-    /* visual */
-    border-radius: 0;
-}
+        /* box */
+        height: 5rem;
+        padding: 0 1.5rem;
+    }
 }
 </style>
