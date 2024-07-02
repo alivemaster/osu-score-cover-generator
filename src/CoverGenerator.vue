13a9f2d5eedbd1f526f0f2da5b3a6d77f7d958f5
alivemaster <alivemaster3@gmail.com>
2024-07-02 22:25:41 +0800

    move out flagIcon function

--- src/CoverGenerator.vue
+++ src/CoverGenerator.vue
@@ -4,9 +4,9 @@ import CoverData from './cover/CoverData'
 import CoverAssets from './cover/CoverAssets'
 import CoverRender from './cover/CoverRender'
 import initAssets from './cover/utils/initAssets'
+import flagIcon from './cover/utils/flagIcon'
 import exportCover from './cover/utils/exportCover'
 import loadImgFile from './utils/loadImgFile'
-import loadImgUrl from './utils/loadImgUrl'
 import countryList from './assets/countries.json'
 import Flex from './components/Flex.vue'
 import Collapsible from './components/Collapsible.vue'
@@ -238,24 +238,6 @@ const renderOptions = reactive({
     type: 'png'
 })
 // Cover methods
-const flagIcon = async (code: string) => {
-    if (code === '')
-        return new Image()
-    else {
-        const codePoints = code
-            .split('')
-            .map((char) => {
-                const offset = 127397
-                const emojiCode = offset + char.charCodeAt(0)
-                const emoji = String.fromCharCode(emojiCode)
-                const point = '1' + emoji.codePointAt(0)!.toString(16)
-                return point
-            })
-        const fileName = codePoints[0] + '-' + codePoints[1]
-        const fileSrc = 'https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/svg/' + fileName + '.svg'
-        return loadImgUrl(fileSrc)
-    }
-}
 const changeAvatar = async (file: File) => {
     coverAssets.user.avatar = await loadImgFile(file)
 }
---
+++ src/cover/utils/flagIcon.ts
@@ -0,0 +1,20 @@
+import loadImgUrl from "../../utils/loadImgUrl"
+
+export default async (code: string) => {
+    if (code === '')
+        return new Image()
+    else {
+        const codePoints = code
+            .split('')
+            .map((char) => {
+                const offset = 127397
+                const emojiCode = offset + char.charCodeAt(0)
+                const emoji = String.fromCharCode(emojiCode)
+                const point = '1' + emoji.codePointAt(0)!.toString(16)
+                return point
+            })
+        const fileName = codePoints[0] + '-' + codePoints[1]
+        const fileSrc = 'https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/svg/' + fileName + '.svg'
+        return loadImgUrl(fileSrc)
+    }
+}
