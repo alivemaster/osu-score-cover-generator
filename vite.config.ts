53d0b0e026bf1c47f288d7fb0f6a636d074e30d7
alivemaster <alivemaster3@gmail.com>
2025-02-25 17:09:36 +0800

    update multiple configuation files

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,19 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    vueDevTools(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    },
   },
   css: {
     postcss: {
